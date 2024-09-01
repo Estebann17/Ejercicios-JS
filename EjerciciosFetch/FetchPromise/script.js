500459821051d2e0be664499367984273d746f89
Estxbann <estebann17e@gmail.com>
2024-09-01 20:45:32 -0300

    promesas

---
+++ EjerciciosFetch/FetchPromise/script.js
@@ -0,0 +1,16 @@
+
+
+// Promesas 
+
+fetch('https://jsonplaceholder.typicode.com/posts') 
+
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+
+.catch((error) => {
+    console.error('Error:', error)
+});
--- EjerciciosFetch/script.js
+++ EjerciciosFetch/script.js
@@ -4,6 +4,32 @@
 // https://jsonplaceholder.typicode.com/posts
 
 
+
+// FOR 
+
+async function fetchPosts() { 
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts'); // Realizo la solicitud asíncrona con la misma url 
+
+        const data = await response.json(); // Obtengo la respuesta a la api y la convierto en un objeto json
+
+        array(data); // Acá llamo a la funcion que hice más abajo llamada array donde paso los datos 
+
+    } catch (error) {
+        console.error('Error: ', error); // El manejo de errores
+    }
+}
+
+function array(data) {  // Acá recorro el array y lo imprimo en consola
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i]); 
+    }
+}
+
+fetchPosts(); // Llamada a la función para que se inicie 
+
+
+
 // FOREACH
 
 async function fetchPosts() {
@@ -12,7 +38,7 @@ async function fetchPosts() {
 
         const data = await response.json();
 
-        data.forEach(post => {
+        data.forEach(post => { // utilizo el array function para hacer el metodo foreach, con la funcion llamada post 
             console.log(post);
         });
 
@@ -24,6 +50,7 @@ async function fetchPosts() {
 fetchPosts();
 
 
+
 // MAP 
 
 async function fetchPosts() {
@@ -32,13 +59,13 @@ async function fetchPosts() {
 
         const data = await response.json();
 
-        const datos = data.map(post => post.title);
+        const datos = data.map(post => post.title) // Con map acceso al array de la variable data, y extraigo el titulo de cada post a traves de esta variable llamada post
 
-        console.log(datos);
+        console.log(datos); // imprimo la variable donde guarde la array function 
 
     } catch (error) {
         console.error('Error: ', error);
     }
 }
 
-fetchPosts();
+fetchPosts(); 
